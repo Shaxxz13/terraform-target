dd3f49d11217941c713698c0b179ab2bfaf08d20
Shaxxz13 <shubhukalra97@gmail.com>
2023-08-02 16:09:56 +0530

    first run

---
+++ tf-target.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Path to the Terraform configuration directory
+TF_DIR="/path/to/terraform/directory"
+
+# Name of the specific .tf file you want to target
+TARGET_FILE="example.tf"
+
+# Get a list of all resources and modules in the target file
+TARGETS=$(terraform state list -state="$TF_DIR/terraform.tfstate" | grep -E "^$TARGET_FILE:.*")
+
+# Loop through each target and run terraform apply -target
+for target in $TARGETS; do
+  terraform apply -target="$target" "$TF_DIR"
+done
